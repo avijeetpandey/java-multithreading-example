0fb778fe91e1c4e962fcff3a783191f942379c48
Avijeet <avijeetpandey25@gmail.com>
2025-02-24 21:24:20 +0530

    add: threading methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         Thread thread = new Thread(() -> {
-            System.out.println("We are in thread" + Thread.currentThread().getName());
+            System.out.println("We are in thread " + Thread.currentThread().getName());
+            throw new RuntimeException("Intentional Exception");
+        });
+
+        // setting thread name
+        thread.setName("New Worker thread");
+
+        // setting thread priority
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        // catching exception on the thread
+        thread.setUncaughtExceptionHandler((t, e) -> {
+            System.out.println("A critical error has been occured " + e.getMessage() + "thread name -> " + t.getName());
         });
 
         // start the thread
